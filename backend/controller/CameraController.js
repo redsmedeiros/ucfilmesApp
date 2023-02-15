c045d2dcf6af5bb449ee2693d2d0e4c4050a1fe4
Rodolpho <redsmedeiros@gmail.com>
2023-02-15 09:41:46 -0300

    Camera controllers

--- backend/controller/CameraController.js
+++ backend/controller/CameraController.js
@@ -55,4 +55,89 @@ module.exports = class CameraController{
         }
 
     }
+
+    static async show(req, res){
+
+        const { id } = req.params
+
+       try{
+
+        const camera = await Camera.findById(id)
+
+        res.status(200).json({camera})
+
+       }catch(error){
+
+        res.send(error)
+
+       }
+
+    }
+
+    static async update(req, res){
+
+        const { id } = req.params
+
+        const { name, description, price, brand, available } = req.body
+
+        let camera = {}
+
+        if(!camera){
+            res.status(404).json({message: 'Camera não encontrada'})
+            return;
+        }
+        
+        //validações
+        if(name){
+            camera.name = name
+        }
+
+        if(description){
+            camera.description = description
+        }
+
+        if(price){
+            camera.price = price
+        }
+
+        if(brand){
+            camera.brand = brand
+        }
+
+        if(available){
+            camera.available = available
+        }
+
+        //salvar e atualizar
+        try{
+
+            const cameraUpdated = await Camera.findByIdAndUpdate(id, camera)
+
+            res.status(200).json({cameraUpdated})
+
+         }catch(error){
+
+            res.send(error)
+
+        }
+
+
+    }
+
+    static async delete(req, res){
+
+        const { id } = req.params
+
+        try{
+
+            const cameraDeleted = await Camera.findByIdAndDelete(id)
+
+            res.status(200).json({message: 'Camera deletada com sucesso'})
+
+        }catch(error){
+
+            res.send(error)
+        }
+
+    }
 }
--- backend/routes/CameraRoutes.js
+++ backend/routes/CameraRoutes.js
@@ -5,5 +5,10 @@ const CameraController = require('../controller/CameraController')
 //rotas
 router.post('/register', CameraController.store)
 router.get('/get-cameras', CameraController.index)
+router.get('/:id', CameraController.show)
+router.patch('/:id', CameraController.update)
+router.delete('/:id', CameraController.delete)
+
+
 
 module.exports = router
